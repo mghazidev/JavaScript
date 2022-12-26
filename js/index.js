1439ec659bfc3f20ec49a2a66f9222b6c1b54171
mghazidev <mghazidev@gmail.com>
2022-12-26 17:42:02 +0500

    nested_objects

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const item = {
+    fname: "Anony",
+    last: {
+      lname: "mous",
+      address: {
+        city: 'munich',
+        country: "Germany"
+      }
+    }
+  }
+  console.log(item.fname+item.last.lname+ ' ' +item.last.address.city+' '+item.last.address.country)
