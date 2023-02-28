40a780872021dfa9ad03b74a98a42ff6b9f253c6
mghazidev <mghazidev@gmail.com>
2023-02-28 13:17:10 +0500

    Solving problems

---
+++ js-learning/conditions.js
@@ -0,0 +1,65 @@
+console.clear()
+
+//1 - Write a program that checks if a number is positive, negative, or zero.
+//2 -  Write a program that checks if a number is even or odd.
+//3 - Write a program that checks if a year is a leap year.
+//4 - Write a program that checks if a given string is a palindrome.
+//5 -  Write a program that checks if a given number is prime or not.
+//6 - Write a program that checks if a given string contains only letters and no numbers.
+//7 - Write a program that checks if a given array is sorted in ascending order.
+//8 - Write a program that checks if a given character is a vowel or a consonant.
+//9 - Write a program that checks if a given array contains a specific element.
+//10 - Write a program that checks if a given number is a perfect square.
+
+
+//1 Solution:
+
+// let num = 0;
+
+// if (num > 0) {
+//     console.log(num + "Positive");
+// }
+
+// else if (num < 0 ) {
+//     console.log(num + "Negative");
+// }
+
+// else {
+//     console.log(num + "Zero");
+// }
+
+// console.clear()
+
+// 2 Solution
+
+// let a = 3;
+
+// if (a % 2 == 0) {
+//     console.log(a +"Even.");
+// }
+// else {
+//     console.log(a + " Odd"); 
+// }
+
+// 3rd Solution
+
+// let a = 2023;
+
+// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//   console.log(year + " is a leap year.");
+// } else {
+//   console.log(year + " is not a leap year.");
+// }
+
+//4th solution
+
+// let a = 'lev'
+
+// let reverse = a.split("").reverse().join("");
+
+// if (a === reverse) {
+//     console.log(a +'palindrom')
+// }
+// else {
+//     console.log(a +'not palindrom')
+// }
