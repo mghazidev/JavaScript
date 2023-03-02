788c02050491da30d1a041b931698d835a7b94a9
mghazidev <mghazidev@gmail.com>
2023-03-02 14:00:34 +0500

    Abstraction

--- js-learning/OOP.js
+++ js-learning/OOP.js
@@ -26,3 +26,64 @@
   }
 const another =new Circle(1) //NEW OPERATOR WILL CREATE EMPTY OBJECT THAN SET THIS TO POINT THE OBJECT
 console.log(another)
+
+
+//Abstraction in OOP
+
+ class Car {
+   constructor(color,make,model,year) {
+     this.color = color;
+     this.make = make;
+     this.model = model;
+     this.year = year;
+   }
+
+   start() {
+     console.log(`Starting the ${this.make} ${this.model}`);
+   }
+
+   accelerate() {
+     console.log(`Accelerating the ${this.make} ${this.model}`)
+   }
+
+   stopping() {
+     console.log(`Stopping the ${this.make} ${this.color}`)
+   }
+ } 
+
+ // const myCar = new Car('red','Bmw','M3','2022')
+
+ myCar.start();
+
+//if their are Many fields than we will declare a variable in constructor and add them
+
+class Car {
+  constructor(Details) {
+    this.color = Details.color
+    this.make = Details.make
+    this.model =Details.model
+    this.year = Details.year
+  }
+
+  start() {
+    console.log(`Starting the ${this.make} ${this.model}`);
+  }
+
+  accelerate() {
+    console.log(`Accelerating the ${this.make} ${this.model}`)
+  }
+
+  stopping() {
+    console.log(`Stopping the ${this.make} ${this.color}`)
+  }
+} 
+
+const myCar = new Car({
+  color: 'red',
+  make: 'BMW',
+  model: 'M3',
+  year: 2022,
+})
+
+console.log(myCar.start())
+myCar.accelerate()
